f5fa1f80c6dc7b67dcca49fc9c8345f97d8b3d3a
Samer Dawood <Dawood.Samer@hotmail.com>
2023-01-21 15:08:50 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Lottoprogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lottoLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.resultLabel7 = new System.Windows.Forms.Label();
+            this.rightLabel6 = new System.Windows.Forms.Label();
+            this.rightLabel7 = new System.Windows.Forms.Label();
+            this.resultLabel5 = new System.Windows.Forms.Label();
+            this.resultLabel6 = new System.Windows.Forms.Label();
+            this.rightLabel5 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.dragningBox = new System.Windows.Forms.TextBox();
+            this.dragningLabel = new System.Windows.Forms.Label();
+            this.position7 = new System.Windows.Forms.TextBox();
+            this.position6 = new System.Windows.Forms.TextBox();
+            this.position5 = new System.Windows.Forms.TextBox();
+            this.position4 = new System.Windows.Forms.TextBox();
+            this.position3 = new System.Windows.Forms.TextBox();
+            this.position2 = new System.Windows.Forms.TextBox();
+            this.postion1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lottoLabel
+            // 
+            this.lottoLabel.AutoSize = true;
+            this.lottoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLabel.Location = new System.Drawing.Point(13, 23);
+            this.lottoLabel.Name = "lottoLabel";
+            this.lottoLabel.Size = new System.Drawing.Size(81, 13);
+            this.lottoLabel.TabIndex = 0;
+            this.lottoLabel.Text = "Din Lottorad:";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.Location = new System.Drawing.Point(146, 90);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(32, 13);
+            this.infoLabel.TabIndex = 6;
+            this.infoLabel.Text = "info:";
+            // 
+            // resultLabel7
+            // 
+            this.resultLabel7.AutoSize = true;
+            this.resultLabel7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.resultLabel7.Location = new System.Drawing.Point(430, 132);
+            this.resultLabel7.Name = "resultLabel7";
+            this.resultLabel7.Size = new System.Drawing.Size(13, 13);
+            this.resultLabel7.TabIndex = 5;
+            this.resultLabel7.Text = "7";
+            // 
+            // rightLabel6
+            // 
+            this.rightLabel6.AutoSize = true;
+            this.rightLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightLabel6.Location = new System.Drawing.Point(243, 132);
+            this.rightLabel6.Name = "rightLabel6";
+            this.rightLabel6.Size = new System.Drawing.Size(41, 13);
+            this.rightLabel6.TabIndex = 5;
+            this.rightLabel6.Text = "6 rätt:";
+            // 
+            // rightLabel7
+            // 
+            this.rightLabel7.AutoSize = true;
+            this.rightLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightLabel7.Location = new System.Drawing.Point(389, 132);
+            this.rightLabel7.Name = "rightLabel7";
+            this.rightLabel7.Size = new System.Drawing.Size(41, 13);
+            this.rightLabel7.TabIndex = 5;
+            this.rightLabel7.Text = "7 rätt:";
+            // 
+            // resultLabel5
+            // 
+            this.resultLabel5.AutoSize = true;
+            this.resultLabel5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.resultLabel5.Location = new System.Drawing.Point(165, 132);
+            this.resultLabel5.Name = "resultLabel5";
+            this.resultLabel5.Size = new System.Drawing.Size(13, 13);
+            this.resultLabel5.TabIndex = 5;
+            this.resultLabel5.Text = "5";
+            // 
+            // resultLabel6
+            // 
+            this.resultLabel6.AutoSize = true;
+            this.resultLabel6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.resultLabel6.Location = new System.Drawing.Point(284, 132);
+            this.resultLabel6.Name = "resultLabel6";
+            this.resultLabel6.Size = new System.Drawing.Size(13, 13);
+            this.resultLabel6.TabIndex = 5;
+            this.resultLabel6.Text = "6";
+            // 
+            // rightLabel5
+            // 
+            this.rightLabel5.AutoSize = true;
+            this.rightLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightLabel5.Location = new System.Drawing.Point(124, 132);
+            this.rightLabel5.Name = "rightLabel5";
+            this.rightLabel5.Size = new System.Drawing.Size(41, 13);
+            this.rightLabel5.TabIndex = 5;
+            this.rightLabel5.Text = "5 rätt:";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(332, 58);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(158, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start Lotto";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // dragningBox
+            // 
+            this.dragningBox.Location = new System.Drawing.Point(226, 58);
+            this.dragningBox.Name = "dragningBox";
+            this.dragningBox.Size = new System.Drawing.Size(100, 20);
+            this.dragningBox.TabIndex = 3;
+            // 
+            // dragningLabel
+            // 
+            this.dragningLabel.AutoSize = true;
+            this.dragningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dragningLabel.Location = new System.Drawing.Point(124, 58);
+            this.dragningLabel.Name = "dragningLabel";
+            this.dragningLabel.Size = new System.Drawing.Size(95, 13);
+            this.dragningLabel.TabIndex = 2;
+            this.dragningLabel.Text = "Antal Dragning:";
+            // 
+            // position7
+            // 
+            this.position7.Location = new System.Drawing.Point(496, 20);
+            this.position7.Name = "position7";
+            this.position7.Size = new System.Drawing.Size(60, 20);
+            this.position7.TabIndex = 1;
+            // 
+            // position6
+            // 
+            this.position6.Location = new System.Drawing.Point(430, 20);
+            this.position6.Name = "position6";
+            this.position6.Size = new System.Drawing.Size(60, 20);
+            this.position6.TabIndex = 1;
+            // 
+            // position5
+            // 
+            this.position5.Location = new System.Drawing.Point(364, 20);
+            this.position5.Name = "position5";
+            this.position5.Size = new System.Drawing.Size(60, 20);
+            this.position5.TabIndex = 1;
+            // 
+            // position4
+            // 
+            this.position4.Location = new System.Drawing.Point(298, 20);
+            this.position4.Name = "position4";
+            this.position4.Size = new System.Drawing.Size(60, 20);
+            this.position4.TabIndex = 1;
+            // 
+            // position3
+            // 
+            this.position3.Location = new System.Drawing.Point(232, 20);
+            this.position3.Name = "position3";
+            this.position3.Size = new System.Drawing.Size(60, 20);
+            this.position3.TabIndex = 1;
+            // 
+            // position2
+            // 
+            this.position2.Location = new System.Drawing.Point(166, 20);
+            this.position2.Name = "position2";
+            this.position2.Size = new System.Drawing.Size(60, 20);
+            this.position2.TabIndex = 1;
+            // 
+            // postion1
+            // 
+            this.postion1.Location = new System.Drawing.Point(100, 20);
+            this.postion1.Name = "postion1";
+            this.postion1.Size = new System.Drawing.Size(60, 20);
+            this.postion1.TabIndex = 1;
+            this.postion1.TextChanged += new System.EventHandler(this.postion1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 161);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.resultLabel7);
+            this.Controls.Add(this.lottoLabel);
+            this.Controls.Add(this.rightLabel6);
+            this.Controls.Add(this.postion1);
+            this.Controls.Add(this.rightLabel7);
+            this.Controls.Add(this.position2);
+            this.Controls.Add(this.resultLabel5);
+            this.Controls.Add(this.position3);
+            this.Controls.Add(this.resultLabel6);
+            this.Controls.Add(this.position4);
+            this.Controls.Add(this.rightLabel5);
+            this.Controls.Add(this.position5);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.position6);
+            this.Controls.Add(this.dragningBox);
+            this.Controls.Add(this.position7);
+            this.Controls.Add(this.dragningLabel);
+            this.Name = "Form1";
+            this.Text = "Lotto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lottoLabel;
+        private System.Windows.Forms.TextBox position4;
+        private System.Windows.Forms.TextBox position3;
+        private System.Windows.Forms.TextBox position2;
+        private System.Windows.Forms.TextBox postion1;
+        private System.Windows.Forms.TextBox position7;
+        private System.Windows.Forms.TextBox position6;
+        private System.Windows.Forms.TextBox position5;
+        private System.Windows.Forms.Label resultLabel7;
+        private System.Windows.Forms.Label rightLabel6;
+        private System.Windows.Forms.Label rightLabel7;
+        private System.Windows.Forms.Label resultLabel5;
+        private System.Windows.Forms.Label resultLabel6;
+        private System.Windows.Forms.Label rightLabel5;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox dragningBox;
+        private System.Windows.Forms.Label dragningLabel;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lottoprogram
+{
+    public partial class Form1 : Form
+    {
+         int[] lottoRadNummer = {2, 4, 6, 8, 9, 1, 3 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void postion1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int pos1 = int.Parse(postion1.Text);
+                lottoRadNummer[0] = pos1;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ggg");
+                
+            }
+            
+        }
+
+        private void postion2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int pos2 = int.Parse(position2.Text);
+                lottoRadNummer[1] = pos2;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("333");
+
+            }
+           
+        }
+
+        private void postion3_TextChanged(object sender, EventArgs e)
+        {
+            int pos3 = int.Parse(position3.Text);
+            lottoRadNummer[2] = pos3;
+        }
+
+        private void postion4_TextChanged(object sender, EventArgs e)
+        {
+            int pos4 = int.Parse(position4.Text);
+            lottoRadNummer[3] = pos4;
+        }
+
+        private void postion5_TextChanged(object sender, EventArgs e)
+        {
+            int pos5 = int.Parse(position5.Text);
+            lottoRadNummer[4] = pos5;
+        }
+
+        private void postion6_TextChanged(object sender, EventArgs e)
+        {
+            int pos6 = int.Parse(position6.Text);
+            lottoRadNummer[5] = pos6;
+        }
+
+        private void postion7_TextChanged(object sender, EventArgs e)
+        {
+            int pos7 = int.Parse(position7.Text);
+            lottoRadNummer[6] = pos7;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lottoRadNummer.Length; i++)
+            {
+                MessageBox.Show(lottoRadNummer[i].ToString());
+            }
+
+        }
+
+    }
+
+}
